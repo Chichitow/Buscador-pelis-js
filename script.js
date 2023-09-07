d07f605d23b02594ee25bda16756d1d5ac70b669
Isaias Villarruel <isaiasvillarruel_25@hotmail.com>
2023-09-06 22:01:56 -0300

    Buscador de pelis con JS

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies);
+
+let api_key = '07be114c66542d09c258a7a4c738ba7e';
+let urlBase = 'https://api.themoviedb.org/3/search/movie';
+let urlImg = 'https://image.tmdb.org/t/p/w500'
+
+let resultContainer = document.getElementById('results')
+
+function searchMovies() {
+    let searchInput = document.getElementById('searchInput').value
+
+    resultContainer.innerHTML = 'Cargando...'
+
+    fetch(`${urlBase}?query=${searchInput}&api_key=${api_key}`)
+        .then(response => response.json())
+        .then(response => displayMovies(response.results))
+}
+
+function displayMovies(movies) {
+    resultContainer.innerHTML = ''
+
+    if (movies.length === 0) {
+        resultContainer.innerHTML = '<p>No se encontraron resultados para tu busqueda</p>'
+        return
+    }
+
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div')
+        movieDiv.classList.add('movie')
+
+        let tittle = document.createElement('h2')
+        tittle.textContent = movie.tittle
+
+        let releaseDate = document.createElement('p')
+        releaseDate.textContent = 'La fecha de lanzamiento fue: ' + movie.release_date
+
+        let overview = document.createElement('p')
+        overview.textContent = movie.overview
+
+        let posterPath = urlImg + movie.poster_path
+        let poster = document.createElement('img')
+        poster.src = posterPath
+
+        movieDiv.appendChild(poster)
+        movieDiv.appendChild(tittle)
+        movieDiv.appendChild(releaseDate)
+        movieDiv.appendChild(overview)
+
+        resultContainer.appendChild(movieDiv)
+    })
+}
+
